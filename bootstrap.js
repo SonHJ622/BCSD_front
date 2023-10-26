92c870ab90d585bf2e965703859466aed58a7399
SonHJ22 <hyundol9447@gmail.com>
2023-10-26 17:02:19 +0900

    3week ass

---
+++ bootstrap.js
@@ -0,0 +1,9 @@
+// JavaScript 함수를 사용하여 페이지 이동 처리
+function redirectToLogin() {
+  // login.html로 페이지 이동
+  window.location.href = "login.html";
+}
+function redirectToSignup() {
+  // login.html로 페이지 이동
+  window.location.href = "Create_Account.html";
+}

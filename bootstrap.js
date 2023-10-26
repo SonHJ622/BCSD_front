ea494ce84a65546f5f3e554ff97bedf0a09aa5b7
SonHJ22 <hyundol9447@gmail.com>
2023-10-26 17:04:55 +0900

    index설정

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,7 @@
 // JavaScript 함수를 사용하여 페이지 이동 처리
 function redirectToLogin() {
   // login.html로 페이지 이동
-  window.location.href = "login.html";
+  window.location.href = "index.html";
 }
 function redirectToSignup() {
   // login.html로 페이지 이동
